eb6f899921a688aa10c8c7300e7efb3a20e0c2d2
faizan001 <faizan.technosoft@gmail.com>
2024-12-12 20:13:40 +0500

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,26 @@
 import React from "react";
 
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      key="gtag-script"
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=GTM-PT5T3B8Q"
+    ></script>,
+    <script
+      key="gtag-init"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'GTM-PT5T3B8Q', { "send_page_view": true });
+        `,
+      }}
+    ></script>,
+  ]);
+};
+
 export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
     <noscript key="gtm-noscript">
