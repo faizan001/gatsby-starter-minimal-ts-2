15f3993a6a3727028c3055a4979d772f6fe1bc97
Nawab Khan <nkhan@digitalbi.com>
2024-12-12 20:07:06 +0500

    newfixes

--- src/pages/parcerias.tsx
+++ src/pages/parcerias.tsx
@@ -219,7 +219,8 @@ const PartneshipsPage: React.FC<PageProps> = ({ location }) => {
         </Section>
 
         <section className=" bg-neutral-9 py-20"><StaticImage
-          className="flr-section-bg-img sm:max-h-[860px] opacity-100 absolute inset-0 w-full h-full object-cover opacity-100"
+          className="flr-section-bg-img sm:max-h-[860px] opacity-100 absolute"
+          style={{position:"absolute"}}
           layout="fullWidth"
           alt="Flair - A Flair leva a moda ate ti de imediato."
           src={"../images/bg-pattern.png"}
